752da7852bb5af0e37aa2c1d3ef3aaf2cec7e9c4
Gabor <gabor.szakacs@bitrise.io>
2020-01-28 17:26:37 +0100

    Add FakeNewsFeed

--- newsroom/newsroom_test.go
+++ newsroom/newsroom_test.go
@@ -15,6 +15,16 @@ func (s *StubNewsFeed) TaggedWith(news.Tag) []news.Story {
 	return s.Stories
 }
 
+type FakeNewsFeed struct{}
+
+func (f *FakeNewsFeed) TaggedWith(tag news.Tag) []news.Story {
+	if tag == "tag-with-stories" {
+		return []news.Story{{Title: "Story"}}
+	} else {
+		return []news.Story{}
+	}
+}
+
 func TestCreateReport(t *testing.T) {
 	t.Run("when there are news", func(t *testing.T) {
 		e := newsroom.Editor{}
@@ -31,6 +41,16 @@ func TestCreateReport(t *testing.T) {
 		e := newsroom.Editor{}
 		tag := news.Tag("climate")
 		feed := &StubNewsFeed{Stories: []news.Story{}}
+		err := e.CreateReport(tag, feed)
+		if err == nil {
+			t.Errorf("expected error, got none")
+		}
+	})
+
+	t.Run("when there are no fake news", func(t *testing.T) {
+		e := newsroom.Editor{}
+		tag := news.Tag("won't have stories")
+		feed := &FakeNewsFeed{}
 		err := e.CreateReport(tag, feed)
 		if err == nil {
 			t.Errorf("expected error, got none")
